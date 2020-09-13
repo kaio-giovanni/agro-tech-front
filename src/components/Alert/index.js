edeebb54ccc7b0cf01304cf66500f79c99cb2eac
kaio-giovanni <kaio.giovanni3@gmail.com>
2020-09-13 14:49:43 -0300

    Create Toast for websocket information

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import socketIO from "socket.io-client";
-import { Alert } from 'react-bootstrap';
+import { Toast, Badge } from 'react-bootstrap';
 import config from '../../config/constants';
+import './style.css';
 
 const AlertInfo = (props) => {
     const [show, setShow] = useState(false);
@@ -9,20 +10,48 @@ const AlertInfo = (props) => {
 
     useEffect(() => {
         const socket = socketIO(config.API_URL);
+        let txt = "";
 
         socket.on("new mill", data => {
-            setResponse(data);
+            txt = "A usina " + data.name + " foi cadastrada !";
+            setResponse(txt);
             setShow(true);
         });
+
+        socket.on("new harvest", data => {
+            txt = "A colheita " + data.cod + " foi cadastrada !";
+            setResponse(txt);
+            setShow(true);
+        });
+
+        socket.on("new farm", data => {
+            txt = "A fazenda " + data.name + " foi cadastrada !";
+            setResponse(txt);
+            setShow(true);
+        });
+
+        socket.on("new field", data => {
+            txt = "O campo " + data.cod + " foi cadastrado !";
+            setResponse(txt);
+            setShow(true);
+        });
+
     }, []);
 
     return (
-        <Alert variant="dark" show={show} onClose={() => setShow(false)} dismissible>
-            <Alert.Heading> Novo </Alert.Heading>
-            <p>
-                {response.name}
-            </p>
-        </Alert>
+        <Toast className="my-toast" show={show} onClose={() => setShow(false)} delay={3000} autohide>
+            <Toast.Header>
+                <strong className="mr-auto">
+                    <Badge variant="primary">NEW</Badge> Novo item
+                </strong>
+                <small>now</small>
+            </Toast.Header>
+            <Toast.Body style={{ display: "flex"}}>
+                <p>
+                    {response}
+                </p>
+            </Toast.Body>
+        </Toast>
     );
 }
 
