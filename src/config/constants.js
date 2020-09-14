2b18047bdeec62136dc969fc81eae9e5cb669d37
kaio-giovanni <kaio.giovanni3@gmail.com>
2020-09-14 00:44:21 -0300

    Changing constants

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API_URL: process.env.API_BACKEND_URL,
-    API_KEY_MAPS: process.env.GOOGLE_MAPS_KEY
+    API_URL: "http://localhost:3333",
+    API_KEY_MAPS: "AIzaSyAb2eq_Q6DXxcN26EBOAXnLJkrrTclnq24"
 }
